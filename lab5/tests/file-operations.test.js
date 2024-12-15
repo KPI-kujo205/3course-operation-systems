8e32d4e1424b79becbfd30dfb1e7b387a0d2d05a
kujo205 <kuc8301@gmail.com>
2024-12-15 14:48:47 +0200

    feat: added test for hard links with many dirs

--- lab5/tests/file-operations.test.js
+++ lab5/tests/file-operations.test.js
@@ -24,6 +24,7 @@ describe("fs is being created", () => {
 
 describe("file operations in 1 directory", () => {
 	let fd;
+	const text = "012345678910";
 
 	test("creates a file", () => {
 		fs.create("example.txt");
@@ -40,13 +41,30 @@ describe("file operations in 1 directory", () => {
 	});
 
 	test("writes and reads to a file and from file", () => {
-		const text = "Hello world, this is my first ever file!";
 		fs.write(fd, Buffer.from(text));
 		const read = String(fs.read(fd, 100));
 
 		expect(read).toEqual(text);
 	});
+
+	test("seeks to a position and reads from file", () => {
+		fs.seek(fd, 10);
+		const read = String(fs.read(fd, 100));
+
+		expect(read).toEqual("10");
+		fs.close(fd);
+	});
+
+	test("creates a hard link", () => {
+		fs.create("example2.txt");
+		fs.link("example.txt", "example2.txt");
+
+		const fd = fs.open("example2.txt");
+
+		expect(fs.read(fd, 100)).toEqual(text);
+	});
 });
+
 describe("file operations in many directories", () => {
 	test("creates a dir and file in it and switches to '/'", () => {
 		fs.mkdir("dir1");
@@ -78,6 +96,27 @@ describe("file operations in many directories", () => {
 		expect(read).toEqual(text);
 	});
 
+	test("creates a hard link", () => {
+		fs.cd("/");
+		fs.mkdir("hard_link_dir");
+		fs.cd("hard_link_dir");
+		fs.create("example1.txt");
+
+		fs.cd("/");
+		fs.create("hard_link");
+		fs.link("hard_link_dir/example1.txt", "hard_link");
+
+		expect(fs.currentDirectory.directoryEntries.get("hard_link")).toBeDefined();
+	});
+
+	test("removes a hard link", () => {
+		fs.unlink("hard_link");
+
+		expect(
+			fs.currentDirectory.directoryEntries.get("hard_link"),
+		).toBeUndefined();
+	});
+
 	test("deletes a file with an absolute path", () => {
 		fs.cd("dir1");
 		fs.rm("example.txt");
@@ -85,20 +124,5 @@ describe("file operations in many directories", () => {
 		expect(fs.currentDirectory.directoryEntries.has("example.txt")).toEqual(
 			false,
 		);
-	});
-});
-describe("file operations in many directories", () => {
-	test("creates a dir and file in it and switches to '/'", () => {
-		fs.mkdir("dir1");
-
-		fs.cd("dir1");
-
-		fs.create("example.txt");
-
-		fs.cd("/");
-
-		expect(
-			fs.currentDirectory.directoryEntries.get("example.txt"),
-		).toBeDefined();
 	});
 });
